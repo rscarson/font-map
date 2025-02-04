b00b951ae1a5c7bd6b1ab8a591fab636bc0debf2
Richard Carson <r.carson@calian.com>
2025-02-04 09:51:31 -0500

    Fix readme

--- nerd_font_symbols/src/lib.rs
+++ nerd_font_symbols/src/lib.rs
@@ -1,16 +1,16 @@
-//! # Google Material Symbols Font
+//! # JetbrainsMono Nerd Font
 //!
 //! [![Crates.io](https://img.shields.io/crates/v/nerd_font_symbols.svg)](https://crates.io/crates/nerd_font_symbols/)
 //! [![Build Status](https://github.com/rscarson/font-map/actions/workflows/tests.yml/badge.svg?branch=master)](https://github.com/rscarson/font-map/actions?query=branch%3Amaster)
 //! [![docs.rs](https://img.shields.io/docsrs/nerd_font_symbols)](https://docs.rs/nerd_font_symbols/latest/)
 //! [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/rscarson/nerd_font_symbols/master/LICENSE)
 //!
-//! This crate provides an enum of all the glyphs in the Google Material Symbols font.  
+//! This crate provides an enum of all the glyphs in the JetbrainsMono Nerd Font.  
 //! Additionally, it provides a way to load the font, and QOL features for using the font in iced.
 //!
 //! See <https://www.nerdfonts.com/> for more information
 //!
-//! **I am not affiliated with Google Inc., nor do I have any rights to the Google Material Symbols font.**  
+//! **I am not affiliated with Nerd Fonts, nor do I have any rights to the JetbrainsMono Nerd Font.**  
 //! This crate is published with a copy of the font, and its license, as allowed by the license.
 //!
 //! See [`Icon`] for the list of available icons, including their names, codepoints and a preview image.  
